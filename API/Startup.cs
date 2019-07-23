d0a3c0c06dfc94126d681115435422d672e1f321
Liam Russell <liam@liamr.co>
2019-07-23 21:51:37 +1000

    Add AWS Lambda

--- API/Startup.cs
+++ API/Startup.cs
@@ -18,7 +18,7 @@ namespace LiamRussell.Api {
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddSwaggerGen(c => {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Liam Russell API", Version = "v1" });
                 c.DescribeAllEnumsAsStrings();
